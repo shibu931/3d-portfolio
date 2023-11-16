1e944c853af834519027d9bd7349a2c5476b8a0f
Shippu Kumar <Itzshippu@gmail.com>
2023-11-16 19:17:49 +0530

    First Commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Html} from '@react-three/drei'; 
+
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'/>
+    </div>
+  )
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,115 @@
+import React, { Suspense, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber'
+import Fox from '../models/Fox'
+import { Loader } from '../components/Loader'
+const Contact = () => {
+  const formRef  = useRef(null)
+  const [form, setForm] = useState({ name: '', email: '', message: '' })
+  const [isLoading,setIsLoading] = useState(false);
+  const handleChange = (e) => {
+    setForm({...form,[e.target.name]:e.target.value})
+  }
+  const handleFocus = () => { }
+  const handleBlur = () => { }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true)
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name:form.name,
+        to_name:"Shippu",
+        from_email:form.email,
+        to_email:'contact@myportfolio.com',
+        message:form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(()=>{
+      setIsLoading(false)
+      setForm({name:'',email:'',message:''})
+    }).catch((error)=>{
+      setIsLoading(false)
+      console.log(error)
+    })
+
+  }
+  return (
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get In Touch</h1>
+        <form
+          onSubmit={handleSubmit}
+          className='w-full flex flex-col gap-7 mt-14'
+        >
+          <label className='text-black-500 font-semibold'>Name
+          <input type="text"
+            name="name"
+            className='input'
+            placeholder='John'
+            required
+            value={form.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+          />
+          </label>
+          <label className='text-black-500 font-semibold'>Email
+          <input type="email"
+            name="email"
+            className='input'
+            placeholder='john@gmail.com'
+            required
+            value={form.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+          />
+          </label>
+          <label className='text-black-500 font-semibold'>Message
+          <textarea 
+            name="message"
+            rows={4}
+            className='textarea'
+            placeholder='Let me know how can i help you!'
+            required
+            value={form.message}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+          ></textarea>
+          </label>
+          <button 
+          type='submit' 
+          className='btn' 
+          onFocus={handleFocus}
+          onBlur={handleBlur}>
+            {isLoading ? 'Sesnding...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{position:[0,0,5],
+          fov:75,
+          near:0.1,
+          far:1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0,0,1]}/>
+          <ambientLight intensity={0.5}/>
+          <Suspense fallback={<Loader/>}>
+            <Fox 
+              position={[0.5,0.35,0]}
+              rotation={[12.6,-0.6,0]}
+              scale={[0.5,0.5,0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
